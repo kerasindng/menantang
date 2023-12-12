4f3303d978361e59250f1fe4f00520edca44a372
kerasindng <149295661+kerasindng@users.noreply.github.com>
2023-12-13 06:52:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-python server.py
+#!/bin/bash
+
+# Run the server
+python server.py &
+
+# Wait for the server to start (adjust sleep duration if needed)
+sleep 5
+
+# Run fetch.py
 python fetch.py
+
+# Run start.py
 python start.py
